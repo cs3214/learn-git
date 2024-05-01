474f9caac0635b0371eb8ecd2355f4e5eda6ad47
cs3214 <0jj5d2073292w5p@au.com>
2024-05-01 13:55:16 +0900

    6

--- examDir/exam.js
+++ examDir/exam.js
@@ -1,4 +1,4 @@
-//Q1
+//Q1-1
 let tmp
 
 function min(num1, num2) {
@@ -6,4 +6,31 @@ function min(num1, num2) {
     console.log(tmp);
 }
 min(3, 2)
+
+
+//Q2-1
+function helloWorldNTimes(count){
+    if(count > 0){
+        for (let i = 1; i < count + 1; i++){
+            console.log("Hello World" + i);
+        }
+    }
+}
+
+helloWorldNTimes(0)
+
+
+//Q3-1
+const numbers = [5, 4, 3, 2, 1];
+
+function arrayMin(numbers){
+    let min = numbers[0]
+    console.log(min);
+    for (let i2 = 1; i2 < numbers.length; i2++ ){
+        if(numbers[i] < min){
+            min = numbers[i]
+            console.log(min);
+        }
+    }
+}
 
